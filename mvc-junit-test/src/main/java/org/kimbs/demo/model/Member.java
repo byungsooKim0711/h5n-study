2bb1dbf3e098252644af1a3809434190be3049c2
kbs <vlakd0711@naver.com>
2019-11-20 13:19:32 +0900

    Add domain validation test code

--- mvc-junit-test/src/main/java/org/kimbs/demo/model/Member.java
+++ mvc-junit-test/src/main/java/org/kimbs/demo/model/Member.java
@@ -1,11 +1,16 @@
 package org.kimbs.demo.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Data
@@ -15,9 +20,14 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotEmpty(message = "Name field is required")
+    @Length(max = 36, message = "Should be name field length less than 36")
     private String name;
 
+    @Email(message = "Wrong email format")
     private String email;
 
+    @Min(value = 0, message = "Should be score field value more than 0")
+    @Max(value = 100, message = "Should be score field value less than 100")
     private int score;
 }
