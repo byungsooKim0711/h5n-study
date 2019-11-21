7cfee38e320a507b4f54a64d3df228ae23d90f3b
kbs <vlakd0711@naver.com>
2019-11-21 16:18:42 +0900

    Add IntegrationTests

--- mvc-junit-test/src/test/java/org/kimbs/demo/DemoApplicationTests.java
+++ mvc-junit-test/src/test/java/org/kimbs/demo/DemoApplicationTests.java
@@ -1,13 +1,75 @@
 package org.kimbs.demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kimbs.demo.model.Member;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
     @Test
-    void contextLoads() {
+    @DisplayName("통합테스트 FindAll()")
+    public void testFindAll() {
+        /* arrange */
+
+        /* act */
+        ResponseEntity<List<Member>> members = this.restTemplate.exchange(
+                "http://localhost:" + this.port + "/api/member"
+                , HttpMethod.GET
+                , null
+                , new ParameterizedTypeReference<List<Member>>() {}
+        );
+
+        /* assert */
+        assertEquals(HttpStatus.OK, members.getStatusCode());
+        assertEquals(Collections.emptyList(), members.getBody());
     }
 
+    @Test
+    @DisplayName("통합테스트 createMember")
+    public void testCreate() {
+        /* arrange */
+        Member member = new Member();
+        member.setName("kbs");
+        member.setEmail("kbs@humuson.com");
+        member.setScore(100);
+
+        /* act */
+        ResponseEntity<Member> responseEntity = this.restTemplate.postForEntity(
+                "http://localhost:" + this.port + "/api/member"
+                , member
+                , Member.class
+        );
+
+        /* assert */
+        assertAll(
+                () -> assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode()),
+                () -> assertEquals("/api/member/1", responseEntity.getHeaders().getLocation().getPath()),
+                () -> assertEquals(member.getName(), responseEntity.getBody().getName()),
+                () -> assertEquals(member.getScore(), responseEntity.getBody().getScore()),
+                () -> assertEquals(member.getEmail(), responseEntity.getBody().getEmail())
+        );
+    }
 }
