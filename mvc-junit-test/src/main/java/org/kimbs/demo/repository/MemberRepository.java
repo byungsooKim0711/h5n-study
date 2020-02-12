754ef6048f0945b1b3ba9dc8b28f9955a5b55fe5
kbs <vlakd0711@naver.com>
2020-02-12 10:54:01 +0900

    extends QuerydslPredicateExecutor<T> in repository

--- mvc-junit-test/src/main/java/org/kimbs/demo/repository/MemberRepository.java
+++ mvc-junit-test/src/main/java/org/kimbs/demo/repository/MemberRepository.java
@@ -2,12 +2,13 @@ package org.kimbs.demo.repository;
 
 import org.kimbs.demo.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
 
     List<Member> findByName(String name);
 }
