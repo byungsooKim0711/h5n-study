531e6e7c5f584c8fef326d3b5744fccf4ba479e2
kbs <vlakd0711@naver.com>
2020-02-27 16:49:50 +0900

    init query dsl project

---
+++ querydsl-demo/src/test/java/org/kimbs/querydsl/QuerydslDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kimbs.querydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
