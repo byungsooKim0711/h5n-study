be596845be0753c9788d95f71f47c9f1f56e7949
kbs <vlakd0711@naver.com>
2020-03-03 18:13:39 +0900

    TODO: 집에서 case when then 테스트

--- querydsl-demo/src/main/java/org/kimbs/querydsl/takes/repository/TakesCustomRepository.java
+++ querydsl-demo/src/main/java/org/kimbs/querydsl/takes/repository/TakesCustomRepository.java
@@ -1,4 +1,10 @@
 package org.kimbs.querydsl.takes.repository;
 
+import org.kimbs.querydsl.takes.domain.Takes;
+
+import java.util.List;
+
 public interface TakesCustomRepository {
+
+    public List<Takes> convertTakesGrade();
 }
--- querydsl-demo/src/main/java/org/kimbs/querydsl/takes/repository/TakesCustomRepositoryImpl.java
+++ querydsl-demo/src/main/java/org/kimbs/querydsl/takes/repository/TakesCustomRepositoryImpl.java
@@ -1,4 +1,33 @@
 package org.kimbs.querydsl.takes.repository;
 
-public class TakesCustomRepositoryImpl implements TakesCustomRepository {
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import org.kimbs.querydsl.takes.domain.Takes;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+import static org.kimbs.querydsl.takes.domain.QTakes.takes;
+
+public class TakesCustomRepositoryImpl extends QuerydslRepositorySupport implements TakesCustomRepository {
+
+    public TakesCustomRepositoryImpl() {
+        super(Takes.class);
+    }
+
+    @Override
+    public List<Takes> convertTakesGrade() {
+        return from(takes)
+                .select(
+                        Projections.fields(takes, new CaseBuilder().when(takes.grade.goe("95")).then("A+")
+                                .when(takes.grade.goe("90")).then("A")
+                                .when(takes.grade.goe("85")).then("B+")
+                                .when(takes.grade.goe("80")).then("B")
+                                .when(takes.grade.goe("75")).then("C+")
+                                .when(takes.grade.goe("70")).then("C")
+                                .when(takes.grade.goe("65")).then("D+")
+                                .when(takes.grade.goe("60")).then("D")
+                                .otherwise("F"))
+                ).fetch();
+    }
 }
