20eee372b7f500953888351f87953134350f6435
kbs <vlakd0711@naver.com>
2020-03-04 12:10:22 +0900

    JPAQueryFactory 가 꼭 필요한 것인가? - QueryDslRepositorySupport 랑 차이는..?

---
+++ querydsl-demo/src/main/java/org/kimbs/querydsl/config/QueryDslConfig.java
@@ -0,0 +1,22 @@
+package org.kimbs.querydsl.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class QueryDslConfig {
+
+    private final EntityManager em;
+
+    public QueryDslConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(em);
+    }
+}
