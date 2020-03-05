9ff8007a975602654c8d73cd3490bad2724f13d4
kbs <vlakd0711@naver.com>
2020-03-05 16:16:16 +0900

    assert문 추가

--- querydsl-demo/src/test/java/org/kimbs/querydsl/department/repository/DepartmentRepositoryTests.java
+++ querydsl-demo/src/test/java/org/kimbs/querydsl/department/repository/DepartmentRepositoryTests.java
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class DepartmentRepositoryTests {
@@ -23,8 +25,22 @@ public class DepartmentRepositoryTests {
         // act
         List<Department> actual = departmentRepository.findDepartmentFetchJoinStudent();
 
-        actual.forEach(d -> System.out.println("ID=" + d.getId() + " NAME=" + d.getName() + " OFFICE=" + d.getOffice() + " STUDENTS=" + d.getStudents()));
-
         // assert
+        Department d1 = actual.get(0);
+        assertEquals("컴퓨터공학과", d1.getName());
+        assertEquals(3, d1.getStudents().size());
+
+        Department d2 = actual.get(1);
+        assertEquals("산업공학과", d2.getName());
+        assertEquals(3, d2.getStudents().size());
+
+        Department d3 = actual.get(2);
+        assertEquals("전자공학과", d3.getName());
+        assertEquals(2, d3.getStudents().size());
+
+        Department d4 = actual.get(3);
+        assertEquals("응용통계학과", d4.getName());
+        assertEquals(0, d4.getStudents().size());
+
     }
 }
