c994841af289951140bd63d5092add7bd266a5ed
kbs <vlakd0711@naver.com>
2020-03-09 13:03:02 +0900

    init project

--- xunit/src/main/java/org/kimbs/xunit/Assert.java
+++ xunit/src/main/java/org/kimbs/xunit/Assert.java
@@ -3,6 +3,10 @@ package org.kimbs.xunit;
 public class Assert {
 
     public static void assertEquals(Object expected, Object actual) {
+        if (expected == null && actual == null) {
+            return ;
+        }
+
         if (!expected.equals(actual)) {
             throw new AssertionError("expected <" + expected + "> but was <" + actual + ">");
         }
