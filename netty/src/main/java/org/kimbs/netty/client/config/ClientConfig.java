4cbf6f197cc72734fa64e29e49e066a169de907b
kbs <vlakd0711@naver.com>
2020-03-20 13:38:18 +0900

    Configuration 유효성 체크 추가,,

--- netty/src/main/java/org/kimbs/netty/client/config/ClientConfig.java
+++ netty/src/main/java/org/kimbs/netty/client/config/ClientConfig.java
@@ -6,8 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Getter
 @Validated
@@ -25,7 +24,7 @@ public class ClientConfig {
 
     private AuthServer authServer;
 
-    public ClientConfig(String id, String password, int timeoutMs, AuthServer authServer) {
+    public ClientConfig(int timeoutMs, AuthServer authServer) {
         this.timeoutMs = timeoutMs;
         this.authServer = authServer;
     }
@@ -34,14 +33,20 @@ public class ClientConfig {
     @Validated
     @ConstructorBinding
     public static class AuthServer {
+
+        @NotNull(message = "HOST IP 정보를 입력해주세요.")
         @Pattern(regexp = "\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b", message = "HOST IP 정보를 다시 확인해주세요.")
         private String host;
 
+        @Min(value = 1, message = "PORT 번호를 확인해주세요.")
+        @Max(value = 65536, message = "PORT 번호를 확인해주세요.")
         private int port;
 
+        @NotNull(message = "CLIENT_ID 를 입력해주세요.")
         @NotBlank(message = "CLIENT_ID 를 입력해주세요.")
         private String id;
 
+        @NotNull(message = "CLIENT_PASSWORD 를 입력해주세요.")
         @NotBlank(message = "CLIENT_PASSWORD 를 입력해주세요.")
         private String password;
 
