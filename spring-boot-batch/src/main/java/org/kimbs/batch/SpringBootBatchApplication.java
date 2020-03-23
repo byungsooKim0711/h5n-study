108be3e4716cb3d92334d75d1b9dc7c0871aa9ce
kbs <vlakd0711@naver.com>
2020-03-23 16:57:40 +0900

    init spring batch project...

---
+++ spring-boot-batch/src/main/java/org/kimbs/batch/SpringBootBatchApplication.java
@@ -0,0 +1,13 @@
+package org.kimbs.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBatchApplication.class, args);
+	}
+
+}
