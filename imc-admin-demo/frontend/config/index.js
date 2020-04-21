7a8a3ed560121ccdfc79bac6aa8eb0b6a41ecc68
kimbs <vlakd0711@naver.com>
2020-04-22 00:30:12 +0900

    assets 위치 변경

--- imc-admin-demo/frontend/config/index.js
+++ imc-admin-demo/frontend/config/index.js
@@ -44,7 +44,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../../src/main/resources/static'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: 'assets',
     assetsPublicPath: '/',
 
     /**
