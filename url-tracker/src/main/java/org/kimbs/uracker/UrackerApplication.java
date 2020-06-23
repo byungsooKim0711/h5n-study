2b20e8b122f9e3cb966f65226e94efbc875eab84
kbs0711 <vlakd0711@naver.com>
2020-06-23 18:05:23 +0900

    init project

---
+++ url-tracker/src/main/java/org/kimbs/uracker/UrackerApplication.java
@@ -0,0 +1,13 @@
+package org.kimbs.uracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrackerApplication.class, args);
+	}
+
+}
