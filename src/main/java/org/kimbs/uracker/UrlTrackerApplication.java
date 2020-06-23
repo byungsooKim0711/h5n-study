1768d9c335e63d480f8eba068d12441e17169b08
kbs0711 <vlakd0711@naver.com>
2020-06-23 15:28:41 +0900

    delete

---
+++ src/main/java/org/kimbs/uracker/UrlTrackerApplication.java
@@ -0,0 +1,13 @@
+package org.kimbs.uracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlTrackerApplication.class, args);
+	}
+
+}
