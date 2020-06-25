3ccffef52210ecaf7528570cb50277530d16d1ec
kbs0711 <vlakd0711@naver.com>
2020-06-25 09:39:54 +0900

    Add KafkaErrorHandler

--- url-tracker/src/main/java/org/kimbs/uracker/handler/CommonKafkaBatchErrorHandler.java
+++ url-tracker/src/main/java/org/kimbs/uracker/handler/CommonKafkaBatchErrorHandler.java
@@ -1,14 +1,23 @@
 package org.kimbs.uracker.handler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.kafka.listener.BatchErrorHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class CommonKafkaBatchErrorHandler implements BatchErrorHandler {
 
     @Override
-    public void handle(Exception e, ConsumerRecords<?, ?> consumerRecords) {
-
+    public void handle(Exception thrownException, ConsumerRecords<?, ?> records) {
+        for (TopicPartition partition : records.partitions()) {
+            for (ConsumerRecord<?, ?> record : records.records(partition)) {
+                String value = (String) record.value();
+                log.error("error received message='{}' with partition-offset='{}', key='{}'", value, partition, record.key());
+            }
+        }
     }
 }
--- url-tracker/src/main/java/org/kimbs/uracker/handler/CommonKafkaErrorHandler.java
+++ url-tracker/src/main/java/org/kimbs/uracker/handler/CommonKafkaErrorHandler.java
@@ -1,14 +1,19 @@
 package org.kimbs.uracker.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.listener.ErrorHandler;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class CommonKafkaErrorHandler implements ErrorHandler {
 
     @Override
-    public void handle(Exception e, ConsumerRecord<?, ?> consumerRecord) {
+    public void handle(Exception thrownException, ConsumerRecord<?, ?> record) {
+        String value = (String) record.value();
+        int partition = record.partition();
 
+        log.error("error received message='{}' with partition-offset='{}', key='{}'", value, partition, record.key());
     }
 }
