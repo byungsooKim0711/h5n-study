751f9a473103367c84ef15c08b7a215341801a1d
kbs0711 <vlakd0711@naver.com>
2020-07-09 10:41:54 +0900

    PasswordEncoder 기존 레거시 DB에 맞춤..

--- imc-admin-demo/src/main/java/org/kimbs/imc/admin/config/SecurityConfig.java
+++ imc-admin-demo/src/main/java/org/kimbs/imc/admin/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 import javax.servlet.http.HttpSessionEvent;
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final LoginSuccessHandler loginSuccessHandler;
     private final LoginFailureHandler loginFailureHandler;
     private final LogoutSuccessHandlerImpl logoutSuccessHandler;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
     private final ImcUserDetailsService userDetailsService;
 
