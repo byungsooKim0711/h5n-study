6896b127425328975768af425891f3a7dc625cfd
kimbs <vlakd0711@naver.com>
2020-08-20 21:40:47 +0900

    RSocket project init

---
+++ rsocket/src/main/java/org/kimbs/rsocket/RsocketApplication.java
@@ -0,0 +1,20 @@
+package org.kimbs.rsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class RsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+
+    }
+
+}
---
+++ rsocket/src/test/java/org/kimbs/rsocket/RsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kimbs.rsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
