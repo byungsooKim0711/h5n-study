5d56d3d279c2e76e521e71c1ff15070d31b103a0
kimbs <vlakd0711@naver.com>
2020-08-20 22:23:02 +0900

    Init RSocketClient Project

---
+++ rsocket/client/src/main/java/org/kimbs/rsocket/client/RsocketApplication.java
@@ -0,0 +1,12 @@
+package org.kimbs.rsocket.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketApplication.class, args);
+    }
+}
---
+++ rsocket/client/src/test/java/org/kimbs/rsocket/client/RsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kimbs.rsocket.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
