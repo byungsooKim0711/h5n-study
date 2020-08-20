6b43d1b0190cd81cc866b9cadb194a207fcd9e16
kimbs <vlakd0711@naver.com>
2020-08-20 23:32:51 +0900

    Init RSocket Integration project

---
+++ rsocket/integration/src/main/java/org/kimbs/rsocket/integration/RsocketIntegrationApplication.java
@@ -0,0 +1,13 @@
+package org.kimbs.rsocket.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketIntegrationApplication.class, args);
+    }
+
+}
